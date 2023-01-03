f6266eb3a776d306c6c452c49177c090f2fe8d2a
qiantao <qiantao@turingvideo.net>
2023-01-03 02:19:00 -0800

    update

--- media/base/media_constants.cc
+++ media/base/media_constants.cc
@@ -104,7 +104,7 @@ const char kVp8CodecName[] = "VP8";
 const char kVp9CodecName[] = "VP9";
 const char kAv1CodecName[] = "AV1";
 const char kH264CodecName[] = "H264";
-const char kH265CodecName[] = "H265";
+//const char kH265CodecName[] = "H265";
 
 // RFC 6184 RTP Payload Format for H.264 video
 const char kH264FmtpProfileLevelId[] = "profile-level-id";
