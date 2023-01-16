5dd6ebb98beadcb00df430e59124326d54fcff15
qiantao <qiantao@turingvideo.net>
2023-01-15 18:56:07 -0800

    update

--- video/rtp_video_stream_receiver2.h
+++ video/rtp_video_stream_receiver2.h
@@ -38,6 +38,7 @@
 #include "modules/rtp_rtcp/source/rtp_video_header.h"
 #include "modules/rtp_rtcp/source/video_rtp_depacketizer.h"
 #include "modules/video_coding/h264_sps_pps_tracker.h"
+#include "modules/video_coding/h265_vps_sps_pps_tracker.h"
 #include "modules/video_coding/loss_notification_controller.h"
 #include "modules/video_coding/nack_requester.h"
 #include "modules/video_coding/packet_buffer.h"
